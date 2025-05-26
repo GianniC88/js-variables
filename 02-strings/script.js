d4da81549552e8d44ccbeee1e5cd6b3a5b631825
GianniC88 <gianni.colucci.88@gmail.com>
2025-05-26 16:49:05 +0200

    Correggi le variabili e le operazioni per stampare correttamente i valori e il nome completo

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,22 +2,31 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
+let name = "Gianni";
+console.log(name);
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+let number = 2;
+console.log(number);
+let text = "ciao";
+console.log(text);
+let bool;
+console.log(bool);
 
-// 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
+// 3. Sostituisci n"ull nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente
 // in console la stringa con il nome completo
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // NON MODIFICARE QUESTA RIGA
 const firstName = "Jane"; // NON MODIFICARE QUESTA RIGA
 const lastName = "Smith"; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
